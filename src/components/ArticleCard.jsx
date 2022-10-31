1e43ce777f4f579699aa00b4fecf00af412cf46c
clam119 <christopher.lam119@gmail.com>
2022-10-31 16:04:58 +0000

    Added a Grid and auto column from Tailwind to have spacing for the cards.

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function SingleArticle ({title, author, topic, body, comment_count, article_id, votes}) {
     const limitedBodyCount = body.slice(0, 250);
     return (
-        <div className="max-w-5xl px-6 py-16 mx-auto">
-            <article className="space-y-8 dark:bg-gray-800 dark:text-gray-50 bg-slate-50 rounded ">
+        <div className="max-w-5xl px-6 py-16 mx-auto auto-cols-auto">
+            <article className=" px-8 py-8 space-y-8 dark:bg-gray-800 dark:text-gray-50 bg-white rounded ">
                 <div className="space-y-6" >
                     <Link to={`/articles/${article_id}`}><h1 className=" text-black-500 hover:border-blue-700 hover:text-blue-700 text-7xl font-bold md:tracking-tight md:text-5xl ">{title}</h1> </Link>
                     <Link to={`topics/${topic}`}> <h2 className=" mt-4 text-lg text-centre text-black-600 hover:border-blue-700 hover:text-blue-700">Topic: #{topic}</h2> </Link>
