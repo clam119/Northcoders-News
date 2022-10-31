00ced0ae2e9c71e01e8dd913c68305845f6f778e
clam119 <christopher.lam119@gmail.com>
2022-10-31 12:16:39 +0000

    Hardcode current username, real name and image url to Grumpy17 - will add user login functionality with SignIn page later.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,12 +17,13 @@ export default function Header () {
                     </div>
                     <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                     {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-                    <Link to="/" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> HOME </Link>
-                    <Link to="/topics" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> TOPICS </Link>
-                    <Link to="/users" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> USERS </Link>
+                    <Link to="/" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Home </Link>
+                    <Link to="/topics" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Topics </Link>
+                    <Link to="/users" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> Users </Link>
                     </div>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                <p className="border-transparent text-gray-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">Current Logged In As: {username} </p>
                     <button type="button" className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <span className="sr-only">View notifications</span>
                     {/* <!-- Heroicon name: outline/bell --> */}
@@ -36,7 +37,7 @@ export default function Header () {
                     <div>
                         <button type="button" className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                         <span className="sr-only">Open user menu</span>
-                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                        <img className="h-8 w-8 rounded-full" src="https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953" alt={`${username}'s avatar profile.`}/>
                         </button>
                     </div>
                    
@@ -75,14 +76,14 @@ export default function Header () {
             <div className="sm:hidden" id="mobile-menu">
                 <div className="pt-2 pb-3 space-y-1">
                 {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
-                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">HOME</a>
-                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">TOPICS</a>
-                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">USERS</a>
+                <Link to="/" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">HOME</Link>
+                <Link to="/topics" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">TOPICS</Link>
+                <Link to="/users" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">USERS</Link>
                 </div>
                 <div className="pt-4 pb-3 border-t border-gray-200">
                 <div className="flex items-center px-4">
                     <div className="flex-shrink-0">
-                    <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                    <img className="h-10 w-10 rounded-full" src="https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953" alt={`${username}'s avatar profile.`}/>
                     </div>
                     <div className="ml-3">
                     <div className="text-base font-medium text-gray-800">{username}</div>
@@ -96,11 +97,11 @@ export default function Header () {
                     </svg>
                     </button>
                 </div>
-                <div className="mt-3 space-y-1">
+                {/* <div className="mt-3 space-y-1">
                     <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Your Profile</a>
                     <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Settings</a>
                     <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Sign out</a>
-                </div>
+                </div> */}
                 </div>
             </div>
         </nav>
