8002209602a4df88252bd6a4179b49a5e10fab40
clam119 <christopher.lam119@gmail.com>
2022-10-31 15:48:38 +0000

    Add the isLoading pattern and a comment for the future - created sort/topic/order states for later tasks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,26 +4,31 @@ import { useState, useEffect } from 'react';
 
 export default function Home () {
 
-    // States To Track User's Filtering - If any of the sort/order/topic change then re-render the articles displayed
+    // States To Track User's Filtering - If any of the sort/order/topic change then re-render the articles displayed - Refactoring: Keeping current states for later use.
     const [displayedArticles, setDisplayedArticles] = useState([]); 
     const [sort, setSort] = useState('created_at');
     const [order, setOrder] = useState('DESC');
     const [topic, setTopic] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         API.getArticles()
         .then((articlesData) => {
-            console.log(articlesData, '<<Success')
             setDisplayedArticles(articlesData);
+            setIsLoading(false);
         })
     }, [sort, order, topic])
 
-    return(
+    if(isLoading) {
+        return <h1>Loading Articles...</h1>
+    }
+    else return(
         <ul className="bg-slate-100 mx-auto px-4 sm:px-6 lg:px-8">
-           {displayedArticles.map(({ article_id, title, author, topic, body, comment_count, votes }) => {
+           {displayedArticles.map(({ article_id, title, author, topic, body, comment_count, votes }, index) => {
             return (
-                <li>
-                    <SingleArticle key={article_id} title={title} author={author} topic={topic} body={body} comment_count={comment_count} article_id={article_id} votes={votes} />
+                <li key={index} >
+                    <SingleArticle title={title} author={author} topic={topic} body={body} comment_count={comment_count} article_id={article_id} votes={votes} />
                 </li>
             )
            })}
