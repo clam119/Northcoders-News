ce84ea77d921fd0c2c4e546e4d0de7881a16cda6
clam119 <christopher.lam119@gmail.com>
2022-10-31 12:04:07 +0000

    Add a Header, Footer and Home component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserContext } from './components/context/UserContext';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import * as API from './utils/api';
 import './App.css';
+import Home from './components/Home';
 
 export default function App() {
   const [username, setUsername] = useState('Not Logged In');
+  const [usersRealName, setUsersRealName]= useState('Not Logged In');
+  const [usersList, setUsersList] = useState([]);
+
+  useEffect(() => {
+    API.getUsers()
+    .then((fetchedUsers) => {
+      setUsersList(fetchedUsers);
+    });
+  }, [])
+
   return (
-    <UserContext.Provider value={{ username, setUsername}}>
+    <UserContext.Provider value={{ username, setUsername, usersList, setUsersList, usersRealName, setUsersRealName }}>
       <div className="App">
         <BrowserRouter>
-        {/* Header, that is persistent not in Routes,  */}
-          
+          <Header />
+            <Routes>
+              <Route path= "/"     element={<Home/>}></Route>
+              {/* <Route path="/users" element={<Users/>}></Route> */}
+            </Routes>
+          <Footer />
         </BrowserRouter>
       </div>
     </UserContext.Provider>
---
+++ src/components/Header.jsx
@@ -0,0 +1,108 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+
+export default function Header () {
+
+    const {  username, setUsername, usersList, setUsersList, usersRealName, setUsersRealName } = useContext(UserContext);
+
+    return (
+        <nav className="bg-white shadow">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16">
+                <div className="flex">
+                    <div className="flex-shrink-0 flex items-center">
+                    <img className="block lg:hidden h-8 w-auto" src="https://i.ibb.co/K7Nf9TR/nclogo.png" alt="Northcoders Logo"/>
+                    <img className="hidden lg:block h-8 w-auto" src="https://i.ibb.co/K7Nf9TR/nclogo.png" alt="Northcoders Logo"/>
+                    </div>
+                    <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                    {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
+                    <Link to="/" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> HOME </Link>
+                    <Link to="/topics" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> TOPICS </Link>
+                    <Link to="/users" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"> USERS </Link>
+                    </div>
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                    <button type="button" className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span className="sr-only">View notifications</span>
+                    {/* <!-- Heroicon name: outline/bell --> */}
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                    </button>
+
+                    {/* <!-- Profile Picture --> */}
+                    <div className="ml-3 relative">
+                    <div>
+                        <button type="button" className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span className="sr-only">Open user menu</span>
+                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                        </button>
+                    </div>
+                   
+                    </div>
+                </div>
+                <div className="-mr-2 flex items-center sm:hidden">
+                    {/* <!-- Mobile menu button --> */}
+                    <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    {/* <!--
+                        Icon when menu is closed.
+
+                        Heroicon name: outline/menu
+
+                        Menu open: "hidden", Menu closed: "block"
+                    --> */}
+                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    {/* <!--
+                        Icon when menu is open.
+
+                        Heroicon name: outline/x
+
+                        Menu open: "block", Menu closed: "hidden"
+                    --> */}
+                    <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                </div>
+                </div>
+            </div>
+
+            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+            <div className="sm:hidden" id="mobile-menu">
+                <div className="pt-2 pb-3 space-y-1">
+                {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
+                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">HOME</a>
+                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">TOPICS</a>
+                <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">USERS</a>
+                </div>
+                <div className="pt-4 pb-3 border-t border-gray-200">
+                <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                    <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                    </div>
+                    <div className="ml-3">
+                    <div className="text-base font-medium text-gray-800">{username}</div>
+                    <div className="text-sm font-medium text-gray-500">{usersRealName}</div>
+                    </div>
+                    <button type="button" className="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span className="sr-only">View notifications</span>
+                        {/* Hero Icon */}
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                    </button>
+                </div>
+                <div className="mt-3 space-y-1">
+                    <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Your Profile</a>
+                    <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Settings</a>
+                    <a href="#" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Sign out</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
