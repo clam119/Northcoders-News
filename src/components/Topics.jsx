6ca5b36333f71b02166a18f9bf5ac7f8f1dbc5d6
clam119 <christopher.lam119@gmail.com>
2022-10-31 17:43:04 +0000

    Change LI's being returned manually to using a TopicCard component that passes in Slug (topic name) as a prop.

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import * as API from '../utils/api';
+import TopicCard from './TopicCard';
 
 export default function Topics () {
     const [topics, setTopics] = useState([]);
@@ -21,8 +22,8 @@ export default function Topics () {
     else {
         return (
             <ul className="bg-slate-100 mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2">
-                {topics.map(({ slug }) => { return (<li key={slug}><h1>{slug}</h1></li>)})}
-            </ul>
+                {topics.map(({ slug }) => { return <TopicCard slug={slug} />})}
+           </ul>
         )
     }
     
