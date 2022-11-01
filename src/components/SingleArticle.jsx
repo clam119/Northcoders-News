b607c2d3720ccc3e654ba9ef034dafac3795ec58
clam119 <christopher.lam119@gmail.com>
2022-11-01 14:19:21 +0000

    Change the interactivity and hover from the 'Upvotes' and 'Title' section of the Single Article component so that the only interactivity on the article card is being able to navigate to the user's page that posted the article.

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -42,7 +42,7 @@ export default function SingleArticle() {
         <div className="max-w-5xl px-6 py-16 mx-auto auto-cols-auto">
             <article className=" px-8 py-8 space-y-8 dark:bg-gray-800 dark:text-gray-50 bg-white rounded ">
                 <div className="space-y-6" >
-                    <Link to={`/articles/${article_id}`}><h1 className=" text-black-500 hover:border-blue-700 hover:text-blue-700 text-7xl font-bold md:tracking-tight md:text-5xl mp:text-4xl">{title}</h1> </Link>
+                    <h1 className=" text-black-500 text-7xl font-bold md:tracking-tight md:text-5xl mp:text-4xl">{title}</h1> 
                     <h2 className=" mt-4 text-lg text-centre text-black-600 mp:text-base">Topic: #{topic}</h2>
                 </div>
                 <div className="dark:text-gray-100">
@@ -50,10 +50,10 @@ export default function SingleArticle() {
                 </div>
                 <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center sm:flex-row sm:items-center dark:text-gray-400">
                     <div className="flex items-center md:space-x-2">
-                        <Link to={`/users/${author}`}> <p className="text-sm text-black-500 hover:border-blue-700 hover:text-blue-700 mp:text-sm">Posted By: {author}</p> </Link>
+                        <Link to={`/users/${author}`}> <p className="text-sm text-black-500 mp:text-sm">Posted By: {author}</p> </Link>
                     </div>
                     <div className="flex items-center md:space-x-2">
-                        <Link to={`/articles/${article_id}`}> <p className="text-right flex-shrink-0 mt-3 text-sm md:mt-0 text-black-500 hover:border-blue-700 hover:text-blue-700 mp:text-sm">Upvotes: {votes}</p> </Link>
+                        <p className="text-right flex-shrink-0 mt-3 text-sm md:mt-0 text-black-500 mp:text-sm">Upvotes: {votes}</p>
                     </div>
                 </div>
             </article>
