a9746f7477c13f9735547b61fd9763d20f3161d9
clam119 <christopher.lam119@gmail.com>
2022-11-01 12:07:42 +0000

    Add a custom 'mp' config which is the size of general mobile phones (350px width)'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: ["./src/components/*.jsx"],
   theme: {
     extend: {},
+    screens: {
+      'mp': '350px', 
+      // => @media (min-width: 350px) { ... } - iPhone/iPad width
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
