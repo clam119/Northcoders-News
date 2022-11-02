376d28c2df9852b0d1439e7f0e472ad7a32bcf65
clam119 <christopher.lam119@gmail.com>
2022-11-02 12:57:22 +0000

    Add the CommentCard & CommentsDisplay which is just a UL that is passed in the comments made on the useEffect fetch request in SingleArticles and passes down the props to CommentCard that renders LIs.

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+export default function CommentCard( { body, author, votes, created_at }) {
+    
+    return(
+        <div className="max-w-fill px-6 py-16 mx-auto auto-cols-auto bg-slate-50">
+            <article className=" px-8 py-8 space-y-8 dark:bg-gray-800 dark:text-gray-50 bg-white rounded ">
+                <div className="dark:text-gray-100">
+                    <p className="text-left md:text-base mp:text-sm">{body}</p>
+                </div>
+                <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center sm:flex-row sm:items-center dark:text-gray-400">
+                        <div className="flex items-center md:space-x-2">
+                            <Link to={`/users/${author}`}> <p className="text-sm text-black-500 hover:border-blue-700 hover:text-blue-700 mp:text-sm">Posted By: {author}</p> </Link>
+                        </div>
+                        <div className="flex items-center md:space-x-2">
+                            <p className="flex-shrink-0 mt-3 text-sm md:mt-0 text-black-500 mp:text-sm">Posted On: {created_at}</p>
+                        </div>
+                            
+                        <div className="flex items-center md:space-x-2">
+                            <p className="text-right flex-shrink-0 mt-3 text-sm md:mt-0 text-black-500 mp:text-sm">Upvotes: {votes}</p> 
+                        </div>
+                    </div>
+            </article>
+	    <div>
+        </div>
+    </div>
+    )
+}
---
+++ src/components/CommentsDisplay.jsx
@@ -0,0 +1,18 @@
+import CommentCard from "./CommentCard"
+
+export default function CommentsDisplay({ comments, setArticle })  {
+    console.log(comments);
+
+    return (
+        <ul className="max-w-5xl bg-white mx-auto px-0 sm:px-6 lg:px-8 grid">
+        {comments.map(({ comment_id, author, body, article_id, votes, created_at }, index) => {
+        return (
+            <li key={index} >
+                <CommentCard comment_id={comment_id} author={ author } body={ body } article_id={ article_id } votes={ votes } created_at={ created_at } setArticle={setArticle} />
+            </li>
+        )
+        })}
+        </ul>
+    )
+
+}
