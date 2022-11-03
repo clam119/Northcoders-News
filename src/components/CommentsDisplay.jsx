43a22579dca88d140a7128b4beecf3e9a53e481d
clam119 <christopher.lam119@gmail.com>
2022-11-03 12:11:32 +0000

    Change bg-slate-100 to 50 for consistency.

--- src/components/CommentsDisplay.jsx
+++ src/components/CommentsDisplay.jsx
@@ -2,7 +2,7 @@ import CommentCard from "./CommentCard"
 
 export default function CommentsDisplay({ comments })  {
     return (
-    comments.length === 0 ? <p className="max-w-5xl px-6 py-16 mx-auto md:text-base mp:text-sm bg-slate-100">No Comments Found</p>: 
+    comments.length === 0 ? <p className="max-w-5xl px-6 py-16 mx-auto md:text-base mp:text-sm bg-slate-50">No Comments Found</p>: 
         <ul className="max-w-5xl bg-white mx-auto px-0 sm:px-6 lg:px-8 grid">
         {comments.map(({ comment_id, author, body, article_id, votes, created_at }, index) => {
         return (
