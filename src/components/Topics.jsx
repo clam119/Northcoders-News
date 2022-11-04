176bbfb9d5eeeaff6ccf9886808607e5e7f9b679
clam119 <christopher.lam119@gmail.com>
2022-11-04 17:51:20 +0000

    Add error handling to the topics page.

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import * as API from '../utils/api';
+import ErrorPage from './ErrorPage';
 import TopicCard from './TopicCard';
 
 export default function Topics () {
     const [topics, setTopics] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [error, setError] = useState(null);
+    const [hasError, setHasError] = useState(false);
     useEffect(() => {
         setIsLoading(true)
         API.getTopics()
@@ -13,12 +15,20 @@ export default function Topics () {
             setTopics(topicsData);
             setIsLoading(false);
         })
+        .catch((err) => {
+            setError(err);
+            setHasError(true);
+        })
     }, [])
 
     if(isLoading) {
         return <h1> Loading Topics...</h1> 
     }
     
+    if(hasError) {
+        return <ErrorPage />
+    }
+
     else {
         return (
             <ul className="bg-slate-100 mx-auto px-4 sm:px-6 lg:px-8 grid md:grid-cols-2 sm:grid-cols-1">
