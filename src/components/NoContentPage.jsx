3022d1f2b62b0b1b6fa24e832eb2f6322783ba7d
clam119 <christopher.lam119@gmail.com>
2022-11-04 16:02:02 +0000

    Fix naming of class to className

--- src/components/NoContentPage.jsx
+++ src/components/NoContentPage.jsx
@@ -6,11 +6,11 @@ export default function NoContentPage () {
             <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                 <div className="max-w-md text-center">
                     <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
-                        <span class="sr-only">204 - No Content</span>
+                        <span className="sr-only">204 - No Content</span>
                     </h2>
                     <p className="text-2xl font-semibold md:text-3xl">No articles on this topic yet</p>
                     <p className="mt-4 mb-8 dark:text-gray-400">Why don't you write the first article for this topic?</p>
-                    <Link to="/" rel="noopener noreferrer" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
+                    <Link to="/" rel="noopener noreferrer" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
                 </div>
             </div>
         </section>
